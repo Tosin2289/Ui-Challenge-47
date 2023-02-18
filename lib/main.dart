a34e5e32c2629eba8d7bd9c35188b3a632828130
Tosin2289 <ayoolatosin00@gmail.com>
2023-02-18 23:20:27 +0100

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,335 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Nikunj(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Nikunj extends StatefulWidget {
+  const Nikunj({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Nikunj> createState() => _NikunjState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _NikunjState extends State<Nikunj> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(35)),
+        padding: const EdgeInsets.all(13),
+        child: Stack(
+          children: [
+            Positioned(
+                child: Column(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        image: const DecorationImage(
+                            image: AssetImage("assets/gym.png"))),
+                    height: MediaQuery.of(context).size.height / 1.95,
+                    width: MediaQuery.of(context).size.width),
+              ],
+            )),
+            Positioned(
+                top: MediaQuery.of(context).size.height / 65,
+                left: MediaQuery.of(context).size.width / 35,
+                child: Container(
+                    height: MediaQuery.of(context).size.height / 17,
+                    width: MediaQuery.of(context).size.width / 8.3,
+                    decoration: BoxDecoration(
+                        boxShadow: [BoxShadow(color: Colors.grey.shade200)],
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      size: MediaQuery.of(context).size.width / 18,
+                    ))),
+            Positioned(
+                top: MediaQuery.of(context).size.height / 52,
+                right: MediaQuery.of(context).size.width / 35,
+                child: Container(
+                    height: MediaQuery.of(context).size.height / 17,
+                    width: MediaQuery.of(context).size.width / 8.3,
+                    decoration: BoxDecoration(
+                        boxShadow: [BoxShadow(color: Colors.grey.shade200)],
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Icon(
+                      Icons.turned_in_sharp,
+                      size: MediaQuery.of(context).size.width / 15,
+                      color: Colors.orange.shade800,
+                    ))),
+            Positioned(
+                bottom: MediaQuery.of(context).size.height / 2.89,
+                right: MediaQuery.of(context).size.width / 900,
+                child: Container(
+                  padding: const EdgeInsets.all(7),
+                  width: MediaQuery.of(context).size.width / 5.3,
+                  height: MediaQuery.of(context).size.height / 11.3,
+                  decoration: BoxDecoration(
+                      boxShadow: [BoxShadow(color: Colors.deepPurple.shade300)],
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "Fee",
+                        style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.width / 21.5,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        "\$20",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.width / 15.4,
+                            color: Colors.white),
+                      )
+                    ],
+                  ),
+                )),
+            Positioned(
+                bottom: MediaQuery.of(context).size.height / 2.21,
+                right: MediaQuery.of(context).size.width / 900,
+                child: Container(
+                  padding: const EdgeInsets.all(7),
+                  width: MediaQuery.of(context).size.width / 5.3,
+                  height: MediaQuery.of(context).size.height / 11.3,
+                  decoration: BoxDecoration(
+                      boxShadow: [BoxShadow(color: Colors.orange.shade300)],
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        Icons.star_outlined,
+                        size: MediaQuery.of(context).size.width / 22.5,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "4.9",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.width / 15,
+                            color: Colors.white),
+                      )
+                    ],
+                  ),
+                )),
+            Positioned(
+              child: Text(
+                " Lifestyle Pro Excercise",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: MediaQuery.of(context).size.width / 14.9),
+              ),
+              bottom: MediaQuery.of(context).size.height / 2.61,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Positioned(
+                bottom: MediaQuery.of(context).size.height / 2.84,
+                child: Text(
+                  "  2 hours 45 minutes  .  23 Video",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54,
+                      fontSize: MediaQuery.of(context).size.width / 22.7),
+                )),
+            Positioned(
+                bottom: MediaQuery.of(context).size.height / 4.3,
+                child: Container(
+                  padding: const EdgeInsets.all(11.5),
+                  decoration: BoxDecoration(
+                      boxShadow: [BoxShadow(color: Colors.grey.shade200)],
+                      borderRadius: BorderRadius.circular(18)),
+                  height: MediaQuery.of(context).size.height / 10.3,
+                  width: MediaQuery.of(context).size.width / 1.09,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height / 13,
+                        width: MediaQuery.of(context).size.width / 6.5,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(
+                                image: AssetImage("assets/arm.png"),
+                                fit: BoxFit.cover)),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 25,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Muscle fibers type",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize:
+                                    MediaQuery.of(context).size.width / 18.8),
+                          ),
+                          Text(
+                            "6:56 min",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize:
+                                    MediaQuery.of(context).size.width / 27,
+                                color: Colors.black26),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 12,
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.height / 18,
+                        width: MediaQuery.of(context).size.width / 8.5,
+                        child: Icon(
+                          Icons.play_arrow_rounded,
+                          size: MediaQuery.of(context).size.width / 12,
+                          color: Colors.yellow.shade700,
+                        ),
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(color: Colors.orange.shade100)
+                        ], borderRadius: BorderRadius.circular(21)),
+                      )
+                    ],
+                  ),
+                )),
+            Positioned(
+                bottom: MediaQuery.of(context).size.height / 8.5,
+                child: Container(
+                  padding: const EdgeInsets.all(11.5),
+                  decoration: BoxDecoration(
+                      boxShadow: [BoxShadow(color: Colors.grey.shade200)],
+                      borderRadius: BorderRadius.circular(18)),
+                  height: MediaQuery.of(context).size.height / 10.3,
+                  width: MediaQuery.of(context).size.width / 1.09,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height / 13,
+                        width: MediaQuery.of(context).size.width / 6.5,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(
+                                image: AssetImage("assets/push.jpg"),
+                                fit: BoxFit.cover)),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 25,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Pus up 40+ daily",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize:
+                                    MediaQuery.of(context).size.width / 18.8),
+                          ),
+                          Text(
+                            "4:36 min",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize:
+                                    MediaQuery.of(context).size.width / 27,
+                                color: Colors.black26),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 7.4,
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.height / 18,
+                        width: MediaQuery.of(context).size.width / 8.5,
+                        child: Icon(
+                          Icons.play_arrow_rounded,
+                          size: MediaQuery.of(context).size.width / 13,
+                          color: Colors.yellow.shade700,
+                        ),
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(color: Colors.orange.shade100)
+                        ], borderRadius: BorderRadius.circular(21)),
+                      )
+                    ],
+                  ),
+                )),
+            Positioned(
+              bottom: MediaQuery.of(context).size.height / 100,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 12,
+                width: MediaQuery.of(context).size.width / 1.09,
+                decoration: BoxDecoration(
+                    boxShadow: [BoxShadow(color: Colors.deepPurple.shade200)],
+                    borderRadius: BorderRadius.circular(40)),
+                child: OutlinedButton(
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40)))),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                            size: MediaQuery.of(context).size.width / 13,
+                          ),
+                          height: MediaQuery.of(context).size.height / 13.7,
+                          width: MediaQuery.of(context).size.width / 6.8,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              color: Colors.orange),
+                        ),
+                        Text(
+                          "  Unlock Course Now ",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: MediaQuery.of(context).size.width / 19),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios_outlined,
+                          size: MediaQuery.of(context).size.width / 21,
+                          color: Colors.grey.shade400,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios_outlined,
+                          size: MediaQuery.of(context).size.width / 19,
+                          color: Colors.grey.shade300,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios_outlined,
+                          size: MediaQuery.of(context).size.width / 17,
+                          color: Colors.grey.shade200,
+                        ),
+                      ],
+                    )),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
